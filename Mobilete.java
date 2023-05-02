7808a775d9d14f633c18e9ef5c7cf9f183c9c67f
DudaaMoura <mariamourapalmital@gmail.com>
2023-05-02 16:33:23 +0000

    veiculos

---
+++ Mobilete.java
@@ -0,0 +1,10 @@
+public class Mobilete extends Veiculo {
+
+    public Mobilete(String marca, String modelo, String cor, int ano){
+        super(marca, modelo, cor, ano);
+    }
+    public Mobilete(){
+        
+    }
+    
+}
